79b7e0aa6b3b3ed90cf53f839cafb146279e7b9b
Sprocket44 <harrisonpelletier@gmail.com>
2020-07-10 17:02:10 -0400

    Updated project to 2019.4.3f1

--- GMTK-Project/Assets/Scripts/TEst.cs
+++ GMTK-Project/Assets/Scripts/TEst.cs
@@ -7,7 +7,7 @@ public class TEst : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("ScriptChanged");
     }
 
     // Update is called once per frame
